e547fa1ab73beaab0cd3099ce1454456404f0475
unknown <augustogabrielromero-arch>
2025-09-06 20:41:35 -0300

    Inicialización TP05 IES6 RomeroAugusto

---
+++ src/App.java
@@ -0,0 +1,25 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Java es fuertemente TIPADO//
+        System.out.println("Hello, IES N6 INFORMATICA!");
+
+
+
+        // Definición e inicialización// 
+        // Primitivos// 
+        int numeroA = 10;
+        int numeroB = 5;
+        //double numeroD = 4.2; 
+        //Boolean bandera = true;
+        //Char caracter = "c"; 
+
+        //Tipo objeto//
+        //Date fecha; 
+
+        numeroA = 30; 
+
+        String numeroC = "20";
+
+        System.out.println("La SUMA de los numeros es: " + (numeroA + numeroC)); 
+    }
+}
